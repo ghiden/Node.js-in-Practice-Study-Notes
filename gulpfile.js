bd4afdc009cd3c8166249af946ea66a998a86808
Hidenari Nozaki <hidenari@gmail.com>
2015-02-20 10:42:47 +1300

    Add gulp-plumber

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var babel = require('gulp-babel');
 var rename = require('gulp-rename');
 var cache = require('gulp-cached');
 var watch = require('gulp-watch');
+var plumber = require('gulp-plumber');
 
 gulp.task('watch', ['es6'], function() {
   watch('./es6/*.es6', function() {
@@ -13,6 +14,7 @@ gulp.task('watch', ['es6'], function() {
 gulp.task('es6', function() {
   return gulp.src('./es6/*.es6')
     .pipe(cache('es6'))
+    .pipe(plumber())
     .pipe(babel())
     .pipe(rename(function(path) {
       path.extname = '.js';
